afc39cf1e5c7a71b72089297639005fd29cc2822
Fabio Batista <fabio@conectaijui.com.br>
2019-07-26 16:43:32 -0300

    Adiciona "ContinuaListagem"

--- Portal/Client.cs
+++ Portal/Client.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Net;
 using System.Threading.Tasks;
 
@@ -19,17 +19,42 @@ namespace Consyste.Clients.Portal
 
         public async Task<ListagemDocumentos> ListaDocumentos(ModeloDocumento modelo, string filtro, string consulta = null, string[] campos = null)
         {
-            string codModelo = CodigoModelo(modelo);
+            return await ListaDocumentos(CodigoModelo(modelo), filtro, consulta, campos);
+        }
 
-            var uri = $"{Config.UrlBase}/api/v1/{codModelo}/lista/{filtro}?q=" + Uri.EscapeUriString(consulta);
-
-            var req = (HttpWebRequest) HttpWebRequest.Create(uri);
-            var res = (HttpWebResponse) await req.GetResponseAsync();
+        public async Task<ListagemDocumentos> ListaDocumentos(string modelo, string filtro, string consulta = null, string[] campos = null)
+        {
+            var res = await PerformGet($"/api/v1/{modelo}/lista/{filtro}?q=" + Uri.EscapeUriString(consulta));
 
             if (res.StatusCode != HttpStatusCode.OK)
                 throw new ApplicationException($"Erro {res.StatusCode} ao solicitar listagem de documentos");
 
             return ListagemDocumentos.FromJSON(res.GetResponseStream());
+        }
+
+        public async Task<ListagemDocumentos> ContinuaListagem(ModeloDocumento modelo, string token)
+        {
+            return await ContinuaListagem(CodigoModelo(modelo), token);
+        }
+
+        public async Task<ListagemDocumentos> ContinuaListagem(string modelo, string token)
+        {
+            var res = await PerformGet($"/api/v1/{modelo}/lista/continua/{token}");
+
+            if (res.StatusCode != HttpStatusCode.OK)
+                throw new ApplicationException($"Erro {res.StatusCode} ao continuar listagem de documentos");
+
+            return ListagemDocumentos.FromJSON(res.GetResponseStream());
+        }
+
+        private async Task<HttpWebResponse> PerformGet(string uri)
+        {
+            var req = (HttpWebRequest) HttpWebRequest.Create(Config.UrlBase + uri);
+            req.Headers.Add(HttpRequestHeader.UserAgent, "Consyst-e .NET Client 1.0");
+            req.Headers.Add(HttpRequestHeader.Accept, "application/json");
+            req.Headers.Add("X-Consyste-Auth-Token", Config.ApiKey);
+
+            return (HttpWebResponse) await req.GetResponseAsync();
         }
 
         private string CodigoModelo(ModeloDocumento modelo)
@@ -41,7 +66,7 @@ namespace Consyste.Clients.Portal
                 case ModeloDocumento.Cte:
                     return "cte";
                 default:
-                    throw new ArgumentException($"Filtro desconhecido: {modelo}", nameof(modelo));
+                    throw new ArgumentException($"Modelo desconhecido: {modelo}", nameof(modelo));
             }
         }
     }
