9ccdf71510f9762ed70d8db6d12298d5150eeb50
saymont <saymonp.trevisan@gmail.com>
2019-08-16 16:50:30 -0300

    Ajuste task

--- tests/ClientTest.cs
+++ tests/ClientTest.cs
@@ -30,7 +30,7 @@ namespace Consyste.Clients.Portal
         [InlineData(ModeloDocumento.Nfe, FiltroDocumento.Emitidos, "H4sIADCtVl0AA9PV1eXSVbBKsVJIzc0syUzJL+ZSULAqtFJQVweJF2dWpVopGBkYADlcAKThI+0sAAAA")]
         [InlineData(ModeloDocumento.Nfe, FiltroDocumento.Recebidos, "H4sIAHWtVl0AAxXLOQ6DMBBA0d6nmIiCapTBilmmS8ctIi+DcAEWNhRw+kD59PURUSFwYMjixcWQigLgjaGun1DiJQya6MYrH+5825ztyaMtcxTm76P7QAjJH4use-qtk1QmfHrntNG6M9IRUUNDK41XfyolPERxAAAA")]
         [InlineData(ModeloDocumento.Nfe, FiltroDocumento.Todos, "H4sIAAmkVl0AAxXKTQ5AMBBA4X1PMWI9MRr1Mzs7t5BWR1jQaFlweiy-l4eICoE9wxl8SAqAD4Y-pfURBk30IYuXuwsbo715sGlZhbn-9f0IPkzXJvsZxn2W3PiqdU4brRsjDRGV1NVSTuoFIAdepWsAAAA_")]
-		public async void TestListaDocumentosNfeProxPagina(ModeloDocumento modelo, FiltroDocumento filtro, string proxPag)
+		public async Task TestListaDocumentosNfeProxPagina(ModeloDocumento modelo, FiltroDocumento filtro, string proxPag)
         {   
             var res = await Cliente().ListaDocumentos(modelo, filtro);
             
@@ -43,7 +43,7 @@ namespace Consyste.Clients.Portal
         [InlineData(ModeloDocumento.Cte, FiltroDocumento.Emitidos, 0)]
         [InlineData(ModeloDocumento.Cte, FiltroDocumento.Tomados, 0)]
         [InlineData(ModeloDocumento.Cte, FiltroDocumento.Todos, 19)]
-        public async void TestListaDocumentosCteTotal(ModeloDocumento modelo, FiltroDocumento filtro, long total)
+        public async Task TestListaDocumentosCteTotal(ModeloDocumento modelo, FiltroDocumento filtro, long total)
         {   
             string [] campos = new string[2]  {"toma_cnpj", "toma_nome"};
             
@@ -59,7 +59,7 @@ namespace Consyste.Clients.Portal
         [InlineData(ModeloDocumento.Cte, FiltroDocumento.Emitidos, "H4sIAEmuVl0AA9PV1eXSVbBKsVJIzc0syUzJL+ZSULAqtFJQVweJF2dWpVopGBkYADlcAKThI+0sAAAA")]
         [InlineData(ModeloDocumento.Cte, FiltroDocumento.Tomados, "H4sIAHeuVl0AA9PV1eXSVbBKsVIoyc9NTMkv5lJQsCq0UlBXBwkXZ1alWikYGRgAOVwAqgm7kSsAAAA_")]
         [InlineData(ModeloDocumento.Cte, FiltroDocumento.Todos, "H4sIAMGtVl0AAxXLMQ6EIBBA0Z5TjLGwmjgSWd3p7LyFARmzFkoWsNDTq+X7yUdEhcCeIQcfkgLgP0NVvTGtlzBoogdFPNxZ2xjtyaNNv1WYh1fPgeDDfGyy5zDti5TGt71z2mjdGemIqKHvR5pZ3ezwgsltAAAA")]
-        public async void TestListaDocumentosCteProxPagina(ModeloDocumento modelo, FiltroDocumento filtro, string proxPag)
+        public async Task TestListaDocumentosCteProxPagina(ModeloDocumento modelo, FiltroDocumento filtro, string proxPag)
         {   
             var res = await Cliente().ListaDocumentos(modelo, filtro);
             
@@ -70,7 +70,7 @@ namespace Consyste.Clients.Portal
         # region ContinuaListagemNfe
         [Theory()]
         [InlineData(ModeloDocumento.Nfe, 195, "H4sIACFUVF0AAxXKTQ5AMBBA4X1PMWI9MRr1Mzs7t5BWR1jQaFlweiy-l4eICoE9wxl8SAqAD4Y-pfURBk30IYuXuwsbo715sGlZhbn-9f0IPkzXJvsZxn2W3PiqdU4brRsjDRGV1NVSTuoFIAdepWsAAAA_")]
-        public async void TestContinuaListagemNfe(ModeloDocumento modelo, long total, string token)
+        public async Task TestContinuaListagemNfe(ModeloDocumento modelo, long total, string token)
         {
             var res = await Cliente().ContinuaListagem(modelo, token);
 	        
@@ -81,7 +81,7 @@ namespace Consyste.Clients.Portal
         # region ContinuaListagemCte
 		[Theory()]
         [InlineData(ModeloDocumento.Cte, 19, "H4sIAHdUVF0AAxWLuw6AIAwAd76ixrkRH0TSzc2-MGhrdFAi4KBfL453uUNEhUBMkDz7qADoIvhV3F8haLTOUIR7fioXgntodHHbhWj4KfcI7Jf7kDP5aUlSGu7sKtxaa9Z8a133xorp1AfeUTzTawAAAA__")]
-        public async void TestContinuaListagemCte(ModeloDocumento modelo, long total, string token)
+        public async Task TestContinuaListagemCte(ModeloDocumento modelo, long total, string token)
         {
             var res = await Cliente().ContinuaListagem(modelo, token);
             
